4cac88c976475974bd646d8412def8bec535155e
AJ Stars <53529358+ajstars1@users.noreply.github.com>
2024-01-21 22:23:16 +0000

    Customized and deployed

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Ayush Jamwal",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "ayushjamwal10@google.com",
+  gitHub: "ajstars1",
+  instagram: "a.j_stars",
+  linkedIn: "ajstars",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "ajstars3",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/neonLights.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "This must be the place, written on wall with neon lights";
 
 /**
  * Project list
@@ -31,10 +31,10 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My offical Portfolio Site🎉",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    url: "https://www.ajstars.me",
   },
   {
     title: "Web Development for Beginners",
