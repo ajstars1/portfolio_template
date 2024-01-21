84b2ef64b7342ed147b005907042121620d6c24c
AJ Stars <53529358+ajstars1@users.noreply.github.com>
2024-01-21 22:40:33 +0000

    added Education section

---
+++ src/Components/Education.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section id="education" className="min-height">
+      <div className="content">
+        <h2>Education</h2>
+        <p>
+          I am currently pursuing my B.Tech in Computer Science and Engineering
+          from <b>Model Institute of Information Technology</b>, Jammu. I am
+          currently in my 3rd year of college.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
